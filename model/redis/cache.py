65104d8c43d379db864c9c35713063b86fc1710b
Peter Kamau <kamaupeter343@gmail.com>
2026-03-25 13:17:18 +0300

    feat: Integrated the caching strategy for easier recompilation

--- model/redis/cache.py
+++ model/redis/cache.py
@@ -2,7 +2,9 @@
 import json
 from functools import wraps
 
-from flask import g
+from flask import g, request
+
+from model.redis.cache_manager import CacheManager
 
 
 def cache_result(key_prefix, ttl=300 ):
@@ -14,16 +16,19 @@ def cache_result(key_prefix, ttl=300 ):
         def wrapper(*args, **kwargs):
             # Build Cache key from prefix and arguements
             # example "user: 123" for get_user(123)
-            cache_key = f"{key_prefix}:{':'.join(str(a) for a in args)}"
+            module = request.args.get("module", "") if request.args else ""
+            doctype_name = request.view_args.get("doctype_name", module) if request.view_args else module
+            cache_key = f"{key_prefix}:{doctype_name}"
+            cache= CacheManager(g.redis)
             #
-            # Try to get the cached result
-            cached = g.redis.get(cache_key)
+            # Try to get the cached result as keys
+            cached = cache.get(cache_key)
             if cached:
-                return json.loads(cached)
+                return cached
             #
             # Execute function to cache results
             result = func(*args, **kwargs)
-            g.redis.setex(cache_key, ttl, json.dumps(result))
+            cache.set(cache_key, result, ttl=ttl, tags=[doctype_name])
             return result
         return wrapper
     return decorator
